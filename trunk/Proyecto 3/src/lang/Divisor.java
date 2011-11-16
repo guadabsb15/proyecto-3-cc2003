6fffb60d919795fc804bbb8d9291467db740403d
templar996@gmail.com <templar996@gmail.com@afb1113e-46e9-796b-b1ee-3c6de7de834d>
2011-11-16 05:44:57 +0000

    Todas las operaciones aritméticas implementadas

---
+++ trunk/Proyecto 3/src/lang/Divisor.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lang;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ *
+ * @author asus
+ */
+public class Divisor implements Operador {
+     private String[] argumentos;
+    
+    private Stack<Double> stack;
+    
+    public Divisor() {
+        stack = new Stack<Double>();
+    }
+    
+    @Override
+    public double calcular(ArrayList<String> args) throws Exception {
+        double resultado = 0;
+        try {
+            for (int i = args.size()-1; i >= 0; i--) {
+                stack.push(Double.parseDouble(args.get(i)));
+            }  
+            while (stack.size() != 1) {
+                double numerador = stack.pop();
+                double denominador = stack.pop();
+                if (denominador != 0) {
+                    stack.push(numerador/denominador);
+                } else {
+                    throw new Exception("Error: División entre cero");
+                }
+            }
+            return stack.pop();
+        } catch (Exception e) {
+            if (e.getMessage().equals("Error: División entre cero")) throw e;
+            throw new Exception("Parámetro no es un número");
+        }
+        
+    }
+}
--- trunk/Proyecto 3/src/lang/Restador.java
+++ trunk/Proyecto 3/src/lang/Restador.java
@@ -5,31 +5,37 @@
 package lang;
 
 import java.util.ArrayList;
+import java.util.Stack;
+
 
 /**
  *
  * @author asus
  */
-public class Sumador implements Operador {
-    
+public class Restador implements Operador {
     private String[] argumentos;
     
-    public Sumador() {
-        
+    private Stack<Double> stack;
+    
+    public Restador() {
+        stack = new Stack<Double>();
     }
     
     @Override
     public double calcular(ArrayList<String> args) throws Exception {
         double resultado = 0;
         try {
-            for (int i = 0; i < args.size(); i++) {
-                resultado += Double.parseDouble(args.get(i));
+            for (int i = args.size()-1; i >= 0; i--) {
+                stack.push(Double.parseDouble(args.get(i)));
             }  
-            return resultado;
+            while (stack.size() != 1) {
+                double termino = stack.pop()-stack.pop();
+                stack.push(termino);
+            }
+            return stack.pop();
         } catch (Exception e) {
             throw new Exception("Parámetro no es un número");
         }
         
     }
-
 }
