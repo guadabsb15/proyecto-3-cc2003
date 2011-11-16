57f8da75f3428ef9f77be5627c1e0d52b8aeb4c7
guadabsb15 <guadabsb15@gmail.com>
2011-11-16 23:52:58 +0000

    primero

---
+++ proyecto-3-cc2003/src/proyecto/pkg3/cc2003/Proyecto3Cc2003.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.pkg3.cc2003;
+
+/**
+ *
+ * @author BarriosSazo
+ */
+public class Proyecto3Cc2003 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
