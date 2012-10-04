be5ee8d6e467e41350466d2baad18a1027747b29
templar996 <templar996@gmail.com>
2012-10-04 00:57:26 +0000

    Scan básico

--- Proyecto 1/src/src/scanner/CocoFileParser.java
+++ Proyecto 1/src/src/scanner/CocoFileParser.java
@@ -5,6 +5,7 @@
 package src.scanner;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import struct.GenericTree;
 
 /**
@@ -13,18 +14,81 @@ import struct.GenericTree;
  */
 public class CocoFileParser {
     
-    private ArrayList<Token> tokens;
+    private CocoFileScanner lexer;
     
     private GenericTree<Token> parseTree;
     
-    public CocoFileParser(ArrayList<Token> tks) {
-        tokens = tks;
+    private Token currentToken;
+    
+    private Token lookAhead;
+    
+    private String compiler;
+    
+    private LinkedHashMap characters;
+    
+    private LinkedHashMap keywords;
+    
+    private LinkedHashMap tokens;
+    
+    public CocoFileParser(String filename) throws Exception {
+        lexer = new CocoFileScanner(filename);
+        lookAhead = lexer.getToken();
+        consume();
         parseTree = new GenericTree();
     }
     
-   public GenericTree<Token> parse() {
-       
+   public GenericTree<Token> parse() throws Exception { 
+       compile();
+       characters();
        return parseTree;
+   }
+
+    private void compile() throws Exception {
+        matchValue(new Token(Token.RESERVED, "COMPILER"));
+        compiler = matchType(Token.IDENT);
+    }
+
+    private String matchType(int token) throws Exception {
+        if (token == currentToken.type()) {
+            String val = currentToken.value();
+            consume();
+            return val;
+        }
+        else throw new MismatchedTokenException("Expected token type " + token + " , but found type " + currentToken.type() + " instead");
+    }
+    
+    private void matchValue(Token token) throws Exception {
+        if (token.value().equals(currentToken.value())) consume();
+        else throw new MismatchedTokenException("Expected token value " + token.value() + " , but found value " + currentToken.value() + " instead");
+    }
+
+    private void consume() throws Exception {
+        currentToken = lookAhead;
+        lookAhead = lexer.getToken();
+       
+    }
+
+    private void characters() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+   
+   
+   class ParserException extends Exception {
+       public ParserException(String msg) {
+           super(msg);
+       }
+   }
+   
+   class MismatchedTokenException extends ParserException {
+       public MismatchedTokenException(String msg) {
+           super(msg);
+       }
+   }
+   
+   class NoViableAltException extends ParserException {
+       public NoViableAltException(String msg) {
+           super(msg);
+       }
    }
     
 }
